d7dcbf7eef74d20d6cccf5ec67144bfc825fe582
Mhjba <mahjoubaaitalla15@gmail.com>
2023-11-17 08:11:39 -0800

    simple shell 1

--- shell.h
+++ shell.h
@@ -11,5 +11,8 @@
 
 
 void execute(char *line, char **array);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 
 #endif
---
+++ string_function.c
@@ -0,0 +1,61 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns the length of string
+ * @s: string.
+ * Return: length.
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * _strcat - concatenates to strings
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+		i++;
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
+
+/**
+ * _strcmp - performs of tow strings.
+ * @s1: the first string
+ * @s2: the second string
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (i);
+}
